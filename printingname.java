0899d58094c9c383dadf0775d306f970853812b3
mubi <smubeenanjum@gmail.com>
2023-06-09 20:53:18 +0530

    added : display name

---
+++ printingname.java
@@ -0,0 +1,14 @@
+public class printingname {
+	
+
+	
+	public static void main(String[] args) {
+	
+	Scanner scanner = new Scanner(System.in);
+
+	System.out.println("Enter the name : ");
+	String name = scanner.next();
+System.out.print( name);
+	}
+	}
+
