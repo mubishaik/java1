0be2a630fe1f6f727a2044e6569ccec0192c3117
mubi <smubeenanjum@gmail.com>
2023-06-09 20:54:59 +0530

    added : string is equal or not

---
+++ string.java
@@ -0,0 +1,19 @@
+ublic class string {
+	public static void main(String[] args) {
+		String string1 = "mubeena";
+		String string2 = "nipuna";
+
+		if(string1.equals(string2)){
+		System.out.println("string is equal");
+		}else{
+		System.out.println("string not equal");
+		}
+		String string3 = "mubeena";
+
+		if(string1.equals(string3)){
+		System.out.println("string is equal");
+		}else{
+		System.out.println("string not equal");
+		}
+		}
+}
