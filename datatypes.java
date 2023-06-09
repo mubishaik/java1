f92f75bc813ee74fc2d0ffe8a6abbd2036d247c4
mubi <smubeenanjum@gmail.com>
2023-06-09 20:55:45 +0530

    added : datatypes

---
+++ datatypes.java
@@ -0,0 +1,30 @@
+public class datatypes {
+	public static void main(String[] args) {
+			
+			boolean stand = false;
+			System.out.println("boolean " + stand);
+
+			byte bytevalue = 1;
+			System.out.println("byte value is :" + bytevalue);
+
+			short shortvalue = 1234;
+			System.out.println(shortvalue);
+
+			long longvalue = 76543298L;
+			System.out.println("Long value is "+ longvalue);
+
+			float floatvalue = 7.06f;
+			System.out.println("floatvalue is "+ floatvalue);
+
+			double doubleValue = 15565.076;
+			System.out.println("double value is "+ doubleValue);
+
+			char charValue = 's';
+			System.out.println("cahr value is  "+  charValue);
+	
+
+		
+	
+
+}
+}
